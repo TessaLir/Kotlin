a15d3d1efce6b4081644f3067542376c32f9cab0
Валентин Ветюков <finik001@gmail.com>
2019-03-07 09:36:14 +0300

    внесены изменения в Адлаптер, для работы с листом.

--- Kotlin_lesson_002/app/src/main/java/ru/vetukov/tutorial/kotlin_lesson_002/ui/main/NotesRVAdapter.kt
+++ Kotlin_lesson_002/app/src/main/java/ru/vetukov/tutorial/kotlin_lesson_002/ui/main/NotesRVAdapter.kt
@@ -1,15 +1,18 @@
 package ru.vetukov.tutorial.kotlin_lesson_002.ui.main
 
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_note.view.*
 import ru.vetukov.tutorial.kotlin_lesson_002.R
 import ru.vetukov.tutorial.kotlin_lesson_002.data.entity.Note
 
-class NotesRVAdapter: RecyclerView.Adapter<NotesRVAdapter.ViewHolder>() {
+class NotesRVAdapter (val onItemClick : ((Note) -> Unit)? = null)
+    : RecyclerView.Adapter<NotesRVAdapter.ViewHolder>() {
 
     var notes: List<Note> = listOf()
         set(value) {
@@ -30,17 +33,29 @@ class NotesRVAdapter: RecyclerView.Adapter<NotesRVAdapter.ViewHolder>() {
 
     override fun getItemCount() = notes.size
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        fun bind(note: Note) = with(itemView) {
-            item_tv_title.text = note.title
-            item_tv_text.text = note.text
-            itemView.setBackgroundColor(note.color)
-//            Picasso
-//                .with(this.context)
-//                .load(note.picture)
-//                .fit()
-//                .centerCrop()
-//                .into(item_picture)
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        val titleTextVew = itemView.findViewById<TextView>(R.id.item_tv_title)
+        val textTextVew = itemView.findViewById<TextView>(R.id.item_tv_text)
+
+        fun bind(note: Note) = with(note) {
+            titleTextVew.text = title
+            textTextVew.text = text
+
+            val background = when(color){
+                Note.Color.WHITE -> R.color.white
+                Note.Color.YELLOW -> R.color.yellow
+                Note.Color.GREEN -> R.color.green
+                Note.Color.BLUE -> R.color.blue
+                Note.Color.RED -> R.color.red
+                Note.Color.VIOLET -> R.color.violet
+                Note.Color.PINK -> R.color.pink
+            }
+
+            itemView.setBackgroundColor(ContextCompat.getColor(itemView.context, background))
+            itemView.setOnClickListener {
+                onItemClick?.invoke(note)
+            }
         }
     }
 }
