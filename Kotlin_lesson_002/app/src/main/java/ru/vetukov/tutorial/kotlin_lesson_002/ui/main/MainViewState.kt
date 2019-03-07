6442dc529ac103756db72f89cd54bfe6ec0c83c7
Валентин Ветюков <finik001@gmail.com>
2019-03-07 09:35:47 +0300

    Отредактирована модель активности

--- Kotlin_lesson_002/app/src/main/java/ru/vetukov/tutorial/kotlin_lesson_002/ui/main/MainViewState.kt
+++ Kotlin_lesson_002/app/src/main/java/ru/vetukov/tutorial/kotlin_lesson_002/ui/main/MainViewState.kt
@@ -2,4 +2,4 @@ package ru.vetukov.tutorial.kotlin_lesson_002.ui.main
 
 import ru.vetukov.tutorial.kotlin_lesson_002.data.entity.Note
 
-class MainViewState(val notes: List<Note>)
+data class MainViewState(val notes: List<Note>)
