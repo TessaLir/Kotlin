7a642f904cd381e4e75be2ac65a92582014ee916
Валентин Ветюков <finik001@gmail.com>
2019-03-07 09:31:13 +0300

    Отредактирован класс Note для более корректной работы приложения

--- Kotlin_lesson_002/app/src/main/java/ru/vetukov/tutorial/kotlin_lesson_002/data/entity/Note.kt
+++ Kotlin_lesson_002/app/src/main/java/ru/vetukov/tutorial/kotlin_lesson_002/data/entity/Note.kt
@@ -1,3 +1,35 @@
 package ru.vetukov.tutorial.kotlin_lesson_002.data.entity
 
-class Note(val title: String, val text: String, val color: Int,  val picture: String = "https://avatars.mds.yandex.net/get-pdb/1025599/3b432859-a79d-4132-8a5d-9393ea17acc4/s1200?webp=false")
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Parcelize
+data class Note(
+    val id: String,
+    val title: String,
+    val text: String,
+    val color: Color = Color.GREEN,
+    val lastChanged: Date = Date()
+    ) : Parcelable {
+
+    override fun equals(other: Any?): Boolean {
+        if(this === other) return true
+        if(javaClass != other?.javaClass) return false
+
+        other as Note
+
+        if(id != other.id) return false
+        return true
+    }
+
+    enum class Color {
+        WHITE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        RED,
+        VIOLET,
+        PINK
+    }
+}
