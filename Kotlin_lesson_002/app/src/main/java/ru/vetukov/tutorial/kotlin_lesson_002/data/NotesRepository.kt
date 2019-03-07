d2d20549496f9a580fc206e70860639013dac5f7
Валентин Ветюков <finik001@gmail.com>
2019-03-07 09:32:19 +0300

    Внесены изменения репозитория, а так же добавлен методы получения листов и возможность сохранения и добавления в листы новой информации

--- Kotlin_lesson_002/app/src/main/java/ru/vetukov/tutorial/kotlin_lesson_002/data/NotesRepository.kt
+++ Kotlin_lesson_002/app/src/main/java/ru/vetukov/tutorial/kotlin_lesson_002/data/NotesRepository.kt
@@ -1,39 +1,72 @@
 package ru.vetukov.tutorial.kotlin_lesson_002.data
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import ru.vetukov.tutorial.kotlin_lesson_002.data.entity.Note
+import java.util.*
 
 object NotesRepository {
 
-    val notes = listOf(
+    private val notesLiveData = MutableLiveData<List<Note>>()
+
+    val notes = mutableListOf(
         Note(
+            UUID.randomUUID().toString(),
             "Первая заметка",
             "Текст первой заметки. Не очень длинный, но интересный",
-            0xfff06292.toInt()
+            color = Note.Color.RED
         ),
         Note(
+            UUID.randomUUID().toString(),
             "Вторая заметка",
             "Текст второй заметки. Не очень длинный, но интересный",
-            0xff9575cd.toInt()
+            color = Note.Color.BLUE
         ),
         Note(
+            UUID.randomUUID().toString(),
             "Третья заметка",
             "Текст третьей заметки. Не очень длинный, но интересный",
-            0xff64b5f6.toInt()
+            color = Note.Color.GREEN
         ),
         Note(
+            UUID.randomUUID().toString(),
             "Четвертая заметка",
             "Текст четвертой заметки. Не очень длинный, но интересный",
-            0xff4db6ac.toInt()
+            color = Note.Color.PINK
         ),
         Note(
+            UUID.randomUUID().toString(),
             "Пятая заметка",
             "Текст пятой заметки. Не очень длинный, но интересный",
-            0xffb2ff59.toInt()
+            color = Note.Color.VIOLET
         ),
         Note(
+            UUID.randomUUID().toString(),
             "Шестая заметка",
             "Текст шестой заметки. Не очень длинный, но интересный",
-            0xffffeb3b.toInt()
+            color = Note.Color.YELLOW
         )
     )
+
+    init {
+        notesLiveData.value = notes
+    }
+
+    fun getNotes(): LiveData<List<Note>> = notesLiveData
+
+    fun saveNote(note: Note) {
+        addOrReplace(note)
+        notesLiveData.value = notes
+    }
+
+    private fun addOrReplace(note: Note) {
+        for (i in 0 until notes.size) {
+            if (notes[i] == note) {
+                notes[i] = note
+                return
+            }
+        }
+        notes.add(note)
+    }
+
 }
